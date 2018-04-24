993ad5af829c8e3bc77fd42d902869373909342d
Rodrigo Mendes <rcmendes80@gmail.com>
2018-04-24 17:47:50 -0300

    init

---
+++ todo-app/src/components/TodoItem/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Component } from "react";
+import "./index.css";
+
+class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false
+    };
+  }
+
+  componentDidMount = ()=> {
+    this.setState({ isChecked: this.props.item.completed})
+  }
+
+  toggleCheckedState = e => {
+    const { onChangeHandler, index } = this.props;
+    this.setState({ isChecked: !this.state.isChecked });
+
+    onChangeHandler(index, e.target.checked);
+  };
+
+  render() {
+    const {index, item} = this.props;
+    const {isChecked} = this.state;
+    return (
+      <tr>
+        <td>{index}:{item.name}</td>
+        <td>{item.due}</td>
+        <td>
+        <input
+            key={index}
+            name={index}
+            type="checkbox"
+            checked={isChecked}
+            onChange={this.toggleCheckedState}
+        />
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default TodoItem;
