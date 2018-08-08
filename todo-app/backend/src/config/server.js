fa1e91128c62f62d61f8e6bd2637d664796f6292
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2018-08-07 22:02:42 -0300

    implementation of backend.

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -9,6 +9,8 @@ server.use(bodyParser.urlencoded({
 }))
 server.use(bodyParser.json())
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
