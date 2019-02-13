5ad7731d9bf0fa61c99ee5a176c15865f734c30d
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2019-02-13 19:22:16 -0200

    added Conext Consumer example.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,11 +3,17 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-	static contextType = LanguageContext; //property name must be contextType to work!!!
+	renderSubmit(value) {
+		return value === 'english' ? 'Submit' : 'Voorleggen';
+	}
 
 	render() {
-		const text = this.context === 'english' ? 'Submit' : 'Voorleggen'; // this.context contains the context value.
-		return <button className="ui button primary">{text}</button>;
+		//Second way to consume Context data
+		return (
+			<button className="ui button primary">
+				<LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
+			</button>
+		);
 	}
 }
 
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -3,9 +3,10 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
-	static contextType = LanguageContext;
+	static contextType = LanguageContext; //property name must be contextType to work!!!
 	render() {
-		const text = this.context === 'english' ? 'Name' : 'Naam';
+		const text = this.context === 'english' ? 'Name' : 'Naam'; // this.context contains the context value.
+
 		return (
 			<div className="ui field">
 				<label>{text}</label>
